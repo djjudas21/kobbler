5a0ff39c85c75693f6bb78d6d0c801deb1849d6c
Jonathan Gazeley <jonathan.gazeley@fasthosts.com>
2021-11-01 13:34:15 +0000

    Auto lint

--- artifact-backer-upper.py
+++ artifact-backer-upper.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
-import os, sys, logging, argparse, tarfile, socket, shutil, traceback, copy
+import os
+import sys
+import logging
+import argparse
+import tarfile
+import socket
+import shutil
+import traceback
+import copy
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from S3Utils import S3Backup
@@ -9,9 +17,8 @@ import secrets
 import pyAesCrypt
 import yaml
 from kubernetes import client, config
-from kubernetes.client import configuration
 
-logger = None
+LOGGER = None
 
 '''
     NAME: artifact-backer-upper
@@ -22,7 +29,7 @@ logger = None
 
     S3 credentials are read from the shared credentials file, see https://boto3.amazonaws.com/v1/documentation/api/latest/guide/configuration.html
 
-    Backup archive lifecycle is managed by S3 BucketLifecycle policy. 
+    Backup archive lifecycle is managed by S3 BucketLifecycle policy.
 
     Upon a successful backup and upload, node-exporter is updated to reflect the current epoch. This data is available under the metric last_artifact_backup
 
@@ -41,17 +48,17 @@ logger = None
 '''
   CONFIG PARAM: SSL_VERIFY
 
-  Boolean denoting whether or not we verify SSL at the S3 server. 
+  Boolean denoting whether or not we verify SSL at the S3 server.
 '''
 SSL_VERIFY = True
 
-''' 
+'''
   CONFIG PARAM: LOG_FILE
 
   The location of our log files
 
 '''
-LOG_FILE='/var/log/artifact-backer-upper.log'
+LOG_FILE = '/var/log/artifact-backer-upper.log'
 
 
 '''
@@ -59,16 +66,17 @@ LOG_FILE='/var/log/artifact-backer-upper.log'
 
   The number of bytes a logfile can grow before it is rotated
 '''
-MAX_LOGSIZE_BYTES=512000
+MAX_LOGSIZE_BYTES = 512000
 
 
-''' 
+'''
   CONFIG PARAM: MAX_LOG_FILES
 
   The number of logfiles to keep on disk
 
 '''
-MAX_LOG_FILES=5
+MAX_LOG_FILES = 5
+
 
 def drop_nones_inplace(d: dict) -> dict:
     """Recursively drop Nones in dict d in-place and return original dict"""
@@ -76,6 +84,7 @@ def drop_nones_inplace(d: dict) -> dict:
     d.clear()
     d.update(dd)
     return d
+
 
 def drop_nones(d: dict) -> dict:
     """Recursively drop Nones in dict d and return a new dict"""
@@ -86,11 +95,12 @@ def drop_nones(d: dict) -> dict:
         elif isinstance(v, (list, set, tuple)):
             # note: Nones in lists are not dropped
             # simply add "if vv is not None" at the end if required
-            dd[k] = type(v)(drop_nones(vv) if isinstance(vv, dict) else vv 
-                            for vv in v) 
+            dd[k] = type(v)(drop_nones(vv) if isinstance(vv, dict) else vv
+                            for vv in v)
         elif v is not None:
             dd[k] = v
     return dd
+
 
 def kube_metadata_filter(resource):
     '''
@@ -99,10 +109,11 @@ def kube_metadata_filter(resource):
     '''
 
     filtered = copy.deepcopy(resource)
-    metadata_remove = [ 'uid', 'owner_references', 'managed_fields', 'resource_version']
-    annotations_remove = [ 'kopf.zalando.org/last-handled-configuration']
+    metadata_remove = ['uid', 'owner_references',
+                       'managed_fields', 'resource_version']
+    annotations_remove = ['kopf.zalando.org/last-handled-configuration']
 
-    for md in metadata_remove:   
+    for md in metadata_remove:
         try:
             del filtered['metadata'][md]
         except KeyError:
@@ -125,7 +136,6 @@ class BackupArtifacts(S3Backup):
         # init the base class
         S3Backup.__init__(self, **kwargs)
 
-
     def backup_artifacts(self):
         # Load kube context from within cluster
         config.load_incluster_config()
@@ -145,14 +155,14 @@ class BackupArtifacts(S3Backup):
         secrets = v1.list_secret_for_all_namespaces(watch=False)
         for item in secrets.items:
             print("%s\t%s" % (item.metadata.namespace, item.metadata.name))
-            
+
             nsdir = os.path.join(dir, item.metadata.namespace)
             if not os.path.exists(nsdir):
                 os.mkdir(nsdir)
 
             # Export the secret as a dict
             dict = item.to_dict()
-        
+
             # Filter out unwanted attributes
             newdict = kube_metadata_filter(dict)
 
@@ -164,28 +174,28 @@ class BackupArtifacts(S3Backup):
 
         return dir
 
-
     def tar_backup_content(self, filename, sources):
         self.filename = filename
-        logging.info("tarring backup to {} with content {}".format(filename, ",".join(sources)))
+        logging.info("tarring backup to {} with content {}".format(
+            filename, ",".join(sources)))
         tar = tarfile.open(self.filename, "w:gz")
         for item in sources:
             arcname = os.path.basename(item)
             tar.add(item, arcname=arcname)
         tar.close()
         logging.info("tarring complete.")
-        self.status = True        
+        self.status = True
         return filename
 
 
 def encrypt_blob(blob, public_key):
-    ''' 
+    '''
         encrypt_blob
 
         Encrypts a blob using an RSA public key
         Adapted from https://cryptobook.nakov.com/asymmetric-key-ciphers/rsa-encrypt-decrypt-examples
     '''
-    #Import the Public Key and use for encryption using PKCS1_OAEP
+    # Import the Public Key and use for encryption using PKCS1_OAEP
     rsa_key = RSA.importKey(public_key)
     encryptor = PKCS1_OAEP.new(rsa_key)
     encrypted = encryptor.encrypt(blob)
@@ -200,14 +210,14 @@ def generate_otp():
     return '-'.join(secrets.choice(words) for i in range(8))
 
 
-def encrypt_file_rsa(public_key, filename): 
-    logging.info("Encrypt backup {} with key {}".format(filename,public_key))
+def encrypt_file_rsa(public_key, filename):
+    logging.info("Encrypt backup {} with key {}".format(filename, public_key))
     # Use the public key for encryption
     fd = open(public_key, "rb")
     public_key = fd.read()
     fd.close()
 
-    #Our candidate file to be encrypted
+    # Our candidate file to be encrypted
     fd = open(filename, "rb")
     unencrypted_blob = fd.read()
     fd.close()
@@ -232,10 +242,10 @@ def encrypt_file_aes(filename, otp):
 
 
 def update_node_exporter(work_dir, collector_dir):
-    ''' 
+    '''
         update_node_exporter
 
-        Writes the current epoch to a file for node-exporter. 
+        Writes the current epoch to a file for node-exporter.
 
         In order to avoid node-exporter picking up a partially written file, the write is atomic
         being written to work_dir and then moved to the collector_dir after
@@ -245,26 +255,27 @@ def update_node_exporter(work_dir, collector_dir):
             collector_dir (string)  - The path node-exporters textcollector is configured to look at
     '''
     logger.info("Writing backup status for node-exporter")
-    currentEpoch = int((datetime.now() - datetime(1970,1,1)).total_seconds())
+    currentEpoch = int((datetime.now() - datetime(1970, 1, 1)).total_seconds())
     promData = [
         "# HELP last_artifact_backup epoch of the last successful artifact backup",
         "# TYPE last_artifact_backup counter",
-        "last_artifact_backup{{ host=\"{}\" }} {}\n".format(socket.gethostname(), currentEpoch),
+        "last_artifact_backup{{ host=\"{}\" }} {}\n".format(
+            socket.gethostname(), currentEpoch),
     ]
-  
+
     # write prom file atomically
     promfile = open("{}/artifact-backup.prom".format(work_dir), 'w')
     promfile.write("{}\n".format("\n".join(promData)))
     promfile.close()
     shutil.move(
-        "{}/artifact-backup.prom".format(work_dir), 
+        "{}/artifact-backup.prom".format(work_dir),
         "{}/artifact-backup.prom".format(collector_dir)
     )
 
 
 # remove the backup file and any intermediate files created
 def cleanup(backup, intermediate_files):
-    
+
     for item in intermediate_files:
         logging.debug("Cleanup requested for {}".format(item))
         if os.path.isfile(item):
@@ -272,13 +283,14 @@ def cleanup(backup, intermediate_files):
         elif os.path.isdir(item):
             shutil.rmtree(item)
         else:
-            logging.warn("Asked to cleanup {} but it's neither a file nor a directory".format(item))
-    
+            logging.warn(
+                "Asked to cleanup {} but it's neither a file nor a directory".format(item))
+
     backup.remove()
 
 
 def main():
- 
+
     os.chdir(args.work_dir)
 
     backup = BackupArtifacts(
@@ -294,7 +306,7 @@ def main():
             logger.info("This bucket appears to be empty")
             return
         for item in objectsInBucket:
-            logger.info("Backup Object: {} Size: {}".format(item[0], item[1]) )
+            logger.info("Backup Object: {} Size: {}".format(item[0], item[1]))
         return
 
     # remove backups from the bucket
@@ -308,7 +320,8 @@ def main():
     if args.get:
         s3objects = args.get.split(",")
         for item in s3objects:
-            backup.get(remote_key=item, local_file="{}/{}".format(args.work_dir,item))
+            backup.get(remote_key=item,
+                       local_file="{}/{}".format(args.work_dir, item))
         return
 
     # if we get here, we should be performing a backup
@@ -322,7 +335,7 @@ def main():
     content = []
     try:
         snapshot = backup.backup_artifacts()
-        content += [ snapshot, ]
+        content += [snapshot, ]
         # we'll want to bin this once backup completes
         intermediate_files.append(snapshot)
 
@@ -332,7 +345,8 @@ def main():
 
             # Generate a one-time passphrase and save it in a file
             otp = generate_otp()
-            otpfile = "{}/otp-{}.{}.txt".format(args.work_dir, hostname, backup_date_time)
+            otpfile = "{}/otp-{}.{}.txt".format(args.work_dir,
+                                                hostname, backup_date_time)
             with open(otpfile, 'w') as f:
                 f.write(otp)
             intermediate_files.append(otpfile)
@@ -343,14 +357,16 @@ def main():
             for c in content:
                 if os.path.isfile(c):
                     encrypt_content.append(c)
-                    logger.info("Added {} to list of files to encrypt".format(c))
+                    logger.info(
+                        "Added {} to list of files to encrypt".format(c))
                 elif os.path.isdir(c):
                     files = os.listdir(c)
                     for f in files:
                         target = os.path.join(c, f)
                         encrypt_content.append(target)
-                        logger.info("Added {} to list of files to encrypt".format(target))
-            
+                        logger.info(
+                            "Added {} to list of files to encrypt".format(target))
+
             # List of files that have been encrypted
             encrypted_content = []
             for f in encrypt_content:
@@ -367,9 +383,9 @@ def main():
             content = encrypted_content
 
         # tarball the backup content
-        backup_filename = "{}/{}.{}.tgz".format(args.work_dir, hostname, backup_date_time)
+        backup_filename = "{}/{}.{}.tgz".format(
+            args.work_dir, hostname, backup_date_time)
         backup.tar_backup_content(backup_filename, content)
-
 
         # if we failed to backup, moan about it
         if not backup.status:
@@ -379,7 +395,7 @@ def main():
             logging.info("get backup size")
             backup.backup_size()
             logging.info("Backup {} was created".format(backup_filename))
-    
+
         # upload and test if that happened
         if backup.upload():
             logging.info("Backup {} was uploaded".format(backup_filename))
@@ -387,40 +403,54 @@ def main():
             # update_node_exporter(args.work_dir,args.collector_dir)
         # otherwise moan about that
         else:
-            logging.error("Uploading backup {} failed, check logs for details".format(backup_filename))
+            logging.error(
+                "Uploading backup {} failed, check logs for details".format(backup_filename))
             raise RuntimeError("Backup upload failed, check logs")
     except Exception as e:
-         logging.error("Exception caught during backup: {} - {}".format(e, traceback.format_exc()))
+        logging.error(
+            "Exception caught during backup: {} - {}".format(e, traceback.format_exc()))
     finally:
-        cleanup(backup, intermediate_files) 
- 
+        cleanup(backup, intermediate_files)
 
 
-    
 if __name__ == "__main__":
- 
+
     # parse out command line arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--loglevel", required=False, default="INFO", help="Log level, one of INFO,WARN,DEBUG (Default=INFO)")
-    parser.add_argument("--list",required=False, help="List existing backups only", action='store_true')
-    parser.add_argument("--remove", required=False, help="Provide a comma separated list of backup files to remove")
-    parser.add_argument("--get", required=False, help="Get an object from the bucket")
-    parser.add_argument("--s3-endpoint", required=False, default="https://s3.gb.iplatform.1and1.org", help="The S3 endpoint (Default=https://s3.gb.iplatform.1and1.org)")
-    parser.add_argument("--bucket", required=True, help="The bucket name for backups to be uploaded to")
-    parser.add_argument("--expire-days", required=False, default=7, type=int, help="The number of days before backups expire (Default=7)")
-    parser.add_argument("--work-dir", required=False, default="/var/tmp", help="Temporary working directory (Default=/var/tmp)")
-    parser.add_argument("--collector-dir", required=False, default="/opt/node_exporter", help="Node export collector directory (Default=/opt/node_exporter)")
-    parser.add_argument("--encrypt", required=False, help="Encrypt the backup file (Default=False)")
-    parser.add_argument("--publickey", required=False, default="/etc/ssh/ssh_host_rsa_key.pub", help="RSA public key to use for encrypting the backup")
-    parser.add_argument("--hostname", required=False, default=socket.gethostname())
+    parser.add_argument("--loglevel", required=False, default="INFO",
+                        help="Log level, one of INFO,WARN,DEBUG (Default=INFO)")
+    parser.add_argument("--list", required=False,
+                        help="List existing backups only", action='store_true')
+    parser.add_argument("--remove", required=False,
+                        help="Provide a comma separated list of backup files to remove")
+    parser.add_argument("--get", required=False,
+                        help="Get an object from the bucket")
+    parser.add_argument("--s3-endpoint", required=False, default="https://s3.gb.iplatform.1and1.org",
+                        help="The S3 endpoint (Default=https://s3.gb.iplatform.1and1.org)")
+    parser.add_argument("--bucket", required=True,
+                        help="The bucket name for backups to be uploaded to")
+    parser.add_argument("--expire-days", required=False, default=7, type=int,
+                        help="The number of days before backups expire (Default=7)")
+    parser.add_argument("--work-dir", required=False, default="/var/tmp",
+                        help="Temporary working directory (Default=/var/tmp)")
+    parser.add_argument("--collector-dir", required=False, default="/opt/node_exporter",
+                        help="Node export collector directory (Default=/opt/node_exporter)")
+    parser.add_argument("--encrypt", required=False,
+                        help="Encrypt the backup file (Default=False)")
+    parser.add_argument("--publickey", required=False, default="/etc/ssh/ssh_host_rsa_key.pub",
+                        help="RSA public key to use for encrypting the backup")
+    parser.add_argument("--hostname", required=False,
+                        default=socket.gethostname())
 
     args = parser.parse_args()
 
     # setup a rotating logfile handler
     logging.basicConfig(level=args.loglevel)
     logger = logging.getLogger(__name__)
-    handler = RotatingFileHandler(LOG_FILE, maxBytes=MAX_LOGSIZE_BYTES, backupCount=MAX_LOG_FILES)
-    handler.setFormatter(logging.Formatter("%(asctime)s:%(levelname)s -> %(message)s", datefmt="%d-%m-%Y %H:%M:%S"))
+    handler = RotatingFileHandler(
+        LOG_FILE, maxBytes=MAX_LOGSIZE_BYTES, backupCount=MAX_LOG_FILES)
+    handler.setFormatter(logging.Formatter(
+        "%(asctime)s:%(levelname)s -> %(message)s", datefmt="%d-%m-%Y %H:%M:%S"))
     logger.addHandler(handler)
 
     logger.debug("Runtime Args: {}".format(args))
@@ -429,6 +459,5 @@ if __name__ == "__main__":
     if sys.version_info[0] < 3:
         logger.warn("Your python version is less than 3, disabling SSL_VERIFY")
         SSL_VERIFY = False
-
 
     main()
